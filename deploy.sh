165087d9d6177e04da27e3da3fa32477e17833a6
Prasanjit Singh <prasenjit.singh@seamless.se>
2017-02-14 01:03:34 +0530

    permissions-added

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ fi
 # destroy old application
 curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/springboot-sample-31047
 ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c '/root/devops/scripts/clean_docker' "
+ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c '/root/devops/scripts/clean_existing' "
 
 # I know this one is ugly. But it works for now.
 sleep 6
--- push.sh
+++ push.sh
@@ -5,7 +5,6 @@ if [ -z "${1}" ]; then
 else
    version="${1}"
 fi
-ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c '/root/devops/scripts/clean_existing' "
 docker push mesos-dev.inframetic.com:5000/gs-spring-boot-docker:latest
 ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c 'docker pull mesos-dev.inframetic.com:5000/gs-spring-boot-docker:latest' "
 #
