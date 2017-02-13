e4d262c179dca9b4c701804f668205b37400ebe1
Prasanjit Singh <prasenjit.singh@seamless.se>
2017-02-13 20:32:11 +0530

    permissions-added

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ if [ -z "${1}" ]; then
 else
    version="${1}"
 fi
-
+ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c 'docker ps| grep gs-spring-boot-docker | sed 's/\|/ /'|awk '{print $1}' | xargs docker rm -f' "
 docker push mesos-dev.inframetic.com:5000/gs-spring-boot-docker:latest
 ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c 'docker pull mesos-dev.inframetic.com:5000/gs-spring-boot-docker:latest' "
 #
